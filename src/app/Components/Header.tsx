62ab2f073f9ac55ad569ac49ded91d65fce1885c
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-17 20:31:31 +0500

    added link

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
   return (
     <div>
-      
+      <div>
+
+     
+      <Link href={"/explore"} className="text-white text-3xl">Explore
+      </Link>
+      </div>
     </div>
   )
 }
